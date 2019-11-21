c90ae5ce26332bbf487cd1acc7ce10e38fc9b127
Samuel Coleman <smcoleman19@gmail.com>
2019-11-20 20:02:10 -0700

    Add basic table setup for teams and conferences

--- db/migrations/20191120143837_initial.js
+++ db/migrations/20191120143837_initial.js
@@ -1,7 +1,26 @@
 exports.up = function(knex) {
-  
+	return Promise.all([
+    knex.schema.createTable('teams', (table) => {
+  		table.increments('id').primary();
+  		table.string('school');
+  		table.string('mascot');
+  		table.string('conference');
+
+  		table.timestamps(true, true);
+  	}),
+  	knex.schema.createTable('conferences', (table) => {
+  		table.increments('id').primary();
+  		table.string('abbreviation');
+  		table.foreign('name').references('teams.conference');
+
+  		table.timestamps(true, true);
+  	})
+  ])
 };
 
 exports.down = function(knex) {
-  
+  return Promise.all([
+  	knex.schema.dropTable('teams'),
+  	knex.schema.dropTable('conferences')
+  ])
 };
