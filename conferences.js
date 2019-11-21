a8ec75373aa3657c3879eefe22cad07d2e6dfea0
Samuel Coleman <smcoleman19@gmail.com>
2019-11-21 11:35:50 -0700

    Add team arrays to conference data and add seed files

--- conferences.js
+++ conferences.js
@@ -4,205 +4,239 @@ let conferences =
 	id: 1,
 	name: "ACC",
 	short_name: "Atlantic Coast Conference",
-	abbreviation: "ACC"
+	abbreviation: "ACC",
+	teams: ["Clemson", "Louisville", "Wake Forest", "Florida State", "Boston College", "NC State", "Syracuse", "Virginia", "Virginia Tech", "Pittsburg", "Miami", "North Carolina", "Duke", "Georgia Tech"]
 	},
 	{
 	id: 4,
 	name: "Big 12",
 	short_name: "Big 12 Conference",
-	abbreviation: "B12"
+	abbreviation: "B12",
+	teams: ["Baylor", "Oklahoma", "Iowa State", "Texas", "Oklahoma State", "Kansas State", "TCU", "West Virginia", "Texas Tech", "Kansas"]
 	},
 	{
 	id: 5,
 	name: "Big Ten",
 	short_name: "Big Ten Conference",
-	abbreviation: "B1G"
+	abbreviation: "B1G",
+	teams: ["Ohio State", "Penn State", "Michigan", "Indiana", "Wisconsin", "Michigan State", "Maryland", "Rutgers", "Minnesota", "Iowa", "Illinois", "Purdue", "Nebraska", "Northwestern"]
 	},
 	{
 	id: 8,
 	name: "SEC",
 	short_name: "Southeastern Conference",
-	abbreviation: "SEC"
+	abbreviation: "SEC",
+	teams: ["Alabama", "Arkansas", "Auburn", "LSU", "Texas A&M", "Mississippi", "Mississippi State", "Georgia", "Florida", "Tennessee", "Kentucky", "South Carolina", "Missouri", "Vanderbilt"]
 	},
 	{
 	id: 9,
 	name: "Pac-12",
 	short_name: "Pac-12 Conference",
-	abbreviation: "PAC"
+	abbreviation: "PAC",
+	teams: ["Arizona", "Arizona State", "Utah", "Colorado", "UCLA", "USC", "Oregon", "Oregon State", "Washington", "Stanford", "California", "Washington State"]
 	},
 	{
 	id: 12,
 	name: "Conference USA",
 	short_name: "Conference USA",
-	abbreviation: "CUSA"
+	abbreviation: "CUSA",
+	teams: ["Marshall", "Florida Atlantic", "Western Kentucky", "Charlotte", "Florida International", "Middle Tennessee", "Old Dominion", "Louisiana Tech", "Southern Mississippi", "UAB", "North Texas", "UT San Antonio", "Rice", "UTEP"]
 	},
 	{
 	id: 15,
 	name: "Mid-American",
 	short_name: "Mid-American Conference",
-	abbreviation: "MAC"
+	abbreviation: "MAC",
+	teams: ["Akron", "Ball State", "Miami (OH)", "Buffalo", "Ohio", "Kent State", "Bowling Green", "Western Michigan", "Central Michigan", "Toledo", "Northern Illinois", "Eastern Michigan"]
 	},
 	{
 	id: 17,
 	name: "Mountain West",
 	short_name: "Mountain West Conference",
-	abbreviation: "MWC"
+	abbreviation: "MWC",
+	teams: ["Air Force", "Boise State", "Utah State", "Colorado State", "Wyoming", "New Mexico", "San Diego State", "Hawaii", "Nevada", "Fresno State", "San Jose State", "UNLV"]
 	},
 	{
 	id: 18,
 	name: "FBS Independents",
 	short_name: "FBS Independents",
-	abbreviation: "Ind"
+	abbreviation: "Ind",
+	teams: ["Army", "Notre Dame", "Liberty", "BYU", "New Mexico State", "UMass"]
 	},
 	{
 	id: 37,
 	name: "Sun Belt",
 	short_name: "Sun Belt Conference",
-	abbreviation: "SBC"
+	abbreviation: "SBC",
+	teams: ["Appalachian State", "Arkansas State", "Georgia Southern", "Troy", "Coastal Carolina", "UL Lafayette", "Louisiana Monroe", "Texas State", "South Alabama"]
 	},
 	{
 	id: 151,
 	name: "American Athletic",
 	short_name: "American Athletic Conference",
-	abbreviation: "AAC"
+	abbreviation: "AAC",
+	teams: ["Memphis", "SMU", "Navy", "Tulane", "Tulsa", "Houston", "Cincinnati", "UCF", "Temple", "South Florida", "East Carolina", "Connecticut"]
 	},
 	{
 	id: 201,
 	name: "Western",
 	short_name: "Western Conference",
-	abbreviation: "Western"
+	abbreviation: "Western",
+	teams: []
 	},
 	{
 	id: 202,
 	name: "Missouri Valley",
 	short_name: "Missouri Valley Intercollegiate Athletic Association",
-	abbreviation: "MVIAA"
+	abbreviation: "MVIAA",
+	teams: []
 	},
 	{
 	id: 203,
 	name: "Rocky Mountain",
 	short_name: "Rocky Mountain Conference",
-	abbreviation: "RMC"
+	abbreviation: "RMC",
+	teams: []
 	},
 	{
 	id: 204,
 	name: "Southwest",
 	short_name: "Southwest Conference",
-	abbreviation: "SWC"
+	abbreviation: "SWC",
+	teams: []
 	},
 	{
 	id: 205,
 	name: "Pacific",
 	short_name: "Pacific Coast Conference",
-	abbreviation: "PCC"
+	abbreviation: "PCC",
+	teams: []
 	},
 	{
 	id: 206,
 	name: "Southern",
 	short_name: "Southern Conference",
-	abbreviation: "Southern"
+	abbreviation: "Southern",
+	teams: []
 	},
 	{
 	id: 207,
 	name: "Big 6",
 	short_name: "Big 6 Conference",
-	abbreviation: "Big 6"
+	abbreviation: "Big 6",
+	teams: []
 	},
 	{
 	id: 208,
 	name: "Missouri Valley",
 	short_name: "Missouri Valley Conference",
-	abbreviation: "MVC"
+	abbreviation: "MVC",
+	teams: []
 	},
 	{
 	id: 209,
 	name: "Mountain State",
 	short_name: "Mountain State Athletic Conference",
-	abbreviation: "MSAC"
+	abbreviation: "MSAC",
+	teams: []
 	},
 	{
 	id: 210,
 	name: "Big 7",
 	short_name: "Big 7 Conference",
-	abbreviation: "Big 7"
+	abbreviation: "Big 7",
+	teams: []
 	},
 	{
 	id: 211,
 	name: "Skyline",
 	short_name: "Skyline Conference",
-	abbreviation: "Skyline"
+	abbreviation: "Skyline",
+	teams: []
 	},
 	{
 	id: 212,
 	name: "Ivy",
 	short_name: "The Ivy League",
-	abbreviation: "Ivy"
+	abbreviation: "Ivy",
+	teams: []
 	},
 	{
 	id: 213,
 	name: "AAWU",
 	short_name: "Athletic Association of Western Universities",
-	abbreviation: "AAWU"
+	abbreviation: "AAWU",
+	teams: []
 	},
 	{
 	id: 214,
 	name: "Big 8",
 	short_name: "Big 8 Conference",
-	abbreviation: "Big 8"
+	abbreviation: "Big 8",
+	teams: []
 	},
 	{
 	id: 215,
 	name: "Western Athletic",
 	short_name: "Western Athletic Conference",
-	abbreviation: "WAC"
+	abbreviation: "WAC",
+	teams: []
 	},
 	{
 	id: 216,
 	name: "Pac-8",
 	short_name: "Pacific 8 Conference",
-	abbreviation: "Pac-8"
+	abbreviation: "Pac-8",
+	teams: []
 	},
 	{
 	id: 217,
 	name: "PCAA",
 	short_name: "Pacific Coast Athletic Association",
-	abbreviation: "PCAA"
+	abbreviation: "PCAA",
+	teams: []
 	},
 	{
 	id: 218,
 	name: "Southland",
 	short_name: "Southland Conference",
-	abbreviation: "Southland"
+	abbreviation: "Southland",
+	teams: []
 	},
 	{
 	id: 219,
 	name: "SWAC",
 	short_name: "Southwest Athletic Conference",
-	abbreviation: "SWAC"
+	abbreviation: "SWAC",
+	teams: []
 	},
 	{
 	id: 220,
 	name: "Pac-10",
 	short_name: "Pacific 10",
-	abbreviation: "Pac-10"
+	abbreviation: "Pac-10",
+	teams: []
 	},
 	{
 	id: 221,
 	name: "Big West",
 	short_name: "Big West Conference",
-	abbreviation: "BW"
+	abbreviation: "BW",
+	teams: []
 	},
 	{
 	id: 222,
 	name: "Big East",
 	short_name: "Big East Conference",
-	abbreviation: "BE"
+	abbreviation: "BE",
+	teams: []
 	},
 	{
 	id: 223,
 	name: "Border",
 	short_name: "Border Intercollegiate Athletic Association",
-	abbreviation: "BIAA"
+	abbreviation: "BIAA",
+	teams: []
 	}
 ]
 
