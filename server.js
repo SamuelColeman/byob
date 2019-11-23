8dec1eddd92f896bf8063f36b1ba46bdd7bc78f9
Samuel Coleman <smcoleman19@gmail.com>
2019-11-23 09:23:45 -0700

    Add functionality to get specific conference data based on id

--- server.js
+++ server.js
@@ -42,6 +42,21 @@ app.get('/api/v1/teams/:id', (request, response) => {
     });
 });
 
+app.get('/api/v1/conferences/:id', (request, response) => {
+  const { id } = request.params;
+  database('conferences')
+    .where({ id: id })
+    .then(conference => {
+      if (conference.length === 0) {
+        response.status(404).json({ error: 'No Conference Found' });
+      }
+      response.status(200).json(conference[0]);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
 app.listen(app.get('port'), () => {
 	console.log(`App is running on ${app.get('port')}`)
 });
