7cb4bd840998501113bd512c209f2ef65853c00b
Samuel Coleman <smcoleman19@gmail.com>
2019-11-23 09:16:28 -0700

    Add functionality for fetching a specific team based on id

--- server.js
+++ server.js
@@ -27,6 +27,21 @@ app.get('/api/v1/conferences', (request, response) => {
     });
 });
 
+app.get('/api/v1/teams/:id', (request, response) => {
+  const { id } = request.params;
+  database('teams')
+    .where({ id: id })
+    .then(team => {
+      if (team.length === 0) {
+        response.status(404).json({ error: 'No Team Found' });
+      }
+      response.status(200).json(team[0]);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
 app.listen(app.get('port'), () => {
 	console.log(`App is running on ${app.get('port')}`)
 });
